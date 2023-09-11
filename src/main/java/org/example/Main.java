42160b38d170b17cde0b644c8e977ada9cde2f2b
ivponicka <56002673+ivponicka@users.noreply.github.com>
2023-09-11 20:17:16 +0200

    Starting the project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> currencyCodes = new HashMap<Integer, String>()
+    }
+}
